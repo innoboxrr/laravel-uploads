4941284eadf850003d9aa586f856cb4d570fea19
Homero Raul <h.raulvc@gmail.com>
2024-03-09 06:06:56 -0600

    Update DisplayRequest.php

--- src/Http/Requests/Upload/DisplayRequest.php
+++ src/Http/Requests/Upload/DisplayRequest.php
@@ -5,6 +5,7 @@ namespace Innoboxrr\LaravelUploads\Http\Requests\Upload;
 use Innoboxrr\LaravelUploads\Models\Upload;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DisplayRequest extends FormRequest
@@ -51,7 +52,9 @@ class DisplayRequest extends FormRequest
 
     public function handle($upload_id, $filename)
     {
-        $upload = Upload::where('uuid', $upload_id)->firstOrFail();
+        $upload = Cache::remember("uploads.{$upload_id}", 60, function () use ($upload_id) {
+            return Upload::where('uuid', $upload_id)->firstOrFail();
+        });
 
         if (!Storage::disk($upload->disk)->exists($upload->path)) {
             abort(404);
@@ -67,5 +70,6 @@ class DisplayRequest extends FormRequest
             'Cache-Control' => 'max-age=26280000',
         ]);
     }
+
 
 }
