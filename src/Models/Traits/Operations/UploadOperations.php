480dee049d18203052305255492958bdda215db8
Homero Raul <h.raulvc@gmail.com>
2024-05-13 23:09:55 -0600

    Update UploadOperations.php

--- src/Models/Traits/Operations/UploadOperations.php
+++ src/Models/Traits/Operations/UploadOperations.php
@@ -5,6 +5,8 @@ namespace Innoboxrr\LaravelUploads\Models\Traits\Operations;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 trait UploadOperations
 {
@@ -29,6 +31,22 @@ trait UploadOperations
             'uploadable_id' => $uploadableId,
             'user_id' => $userId,
         ];
+    }
+
+    public static function getUploadPath(string $uri)
+    {
+        try {
+            $fakeRequest = Request::create($uri, 'GET');
+            $route = Route::getRoutes()->match($fakeRequest);
+            $parameters = $route->parameters();
+            if (!isset($parameters['upload_uuid'])) {
+                return null;
+            }
+            $upload = self::where('uuid', $parameters['upload_uuid'])->first();
+            return $upload ? $upload->path : null;
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 
 }
